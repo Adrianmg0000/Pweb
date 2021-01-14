8bf4421bd7b832a41428323d41bf86bae19f3be8
iqbalq <a2.1900119@mhs.stmik-sumedang.ac.id>
2021-01-14 09:39:27 +0700

    mengupdate calon_siswa jadi pendaftaran_siswa

--- adrian/config.php
+++ adrian/config.php
@@ -2,7 +2,7 @@
 $server = "localhost";
 $user = "root";
 $password = "";
-$database = "calon_siswa";
+$database = "pendaftaran_siswa";
 $db = mysqli_connect($server, $user, $password, $database);
 if( !$db ){
  die("Gagal terhubung dengan database: " . mysqli_connect_error());
